4b288ad063d7b9e5196fa38cac7708622f2ab37f
ZeuberlichStudio <trifonov.ya@yandex.ru>
2020-03-06 20:01:34 +0300

    Updates

---
+++ precache-manifest.9bbe8fd85efa17196b222f15bcd1416c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02ecb368e33199e511fe24db9e215e6",
+    "url": "/gotocart/index.html"
+  },
+  {
+    "revision": "46dd317749daf75ce50f",
+    "url": "/gotocart/static/css/main.eebaab18.chunk.css"
+  },
+  {
+    "revision": "c5962a41da0d0f8c22b1",
+    "url": "/gotocart/static/js/2.6384ae39.chunk.js"
+  },
+  {
+    "revision": "9617c35fefb3909e96d4735aba67fb26",
+    "url": "/gotocart/static/js/2.6384ae39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46dd317749daf75ce50f",
+    "url": "/gotocart/static/js/main.7fc180cf.chunk.js"
+  },
+  {
+    "revision": "6e87f24d9420ced00b17",
+    "url": "/gotocart/static/js/runtime-main.08670b69.js"
+  },
+  {
+    "revision": "8feb512e78c18175c552af2be6ae2e02",
+    "url": "/gotocart/static/media/ProximaNova-Semibold.8feb512e.woff"
+  },
+  {
+    "revision": "a835600f3a92f7c14c412859df878062",
+    "url": "/gotocart/static/media/bg.a835600f.jpg"
+  },
+  {
+    "revision": "ac791800a11e8dcc33b3ba4a6248b491",
+    "url": "/gotocart/static/media/cart.ac791800.svg"
+  },
+  {
+    "revision": "462240379e2b362c1b9219ff474afb28",
+    "url": "/gotocart/static/media/catalog.46224037.svg"
+  },
+  {
+    "revision": "e3bf30c0d60424b45634d9a26c9baaec",
+    "url": "/gotocart/static/media/favourite.e3bf30c0.svg"
+  },
+  {
+    "revision": "c2bb94489436b8c3b3a85caf7867f378",
+    "url": "/gotocart/static/media/logo.c2bb9448.svg"
+  },
+  {
+    "revision": "4ae2d72cc23435df6271c521ec14e753",
+    "url": "/gotocart/static/media/sales.4ae2d72c.svg"
+  },
+  {
+    "revision": "f49b07227656cc0000d03ec60e0b3f2f",
+    "url": "/gotocart/static/media/search.f49b0722.svg"
+  }
+]);
